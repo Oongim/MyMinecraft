a53861d2707f8f465632d60a187bb8d538ef024c
Oongim <43131702+Oongim@users.noreply.github.com>
2021-03-06 00:32:10 +0900

    Default Glut Setting
    가장 기본적인 glut가 돌아가는 프로젝트

---
+++ MyMinecraftClient/MyMinecraftClient/Main.cpp
@@ -0,0 +1,63 @@
+#include "global.h"
+//#include "Dependencies/glew.h"
+#include <GL/freeglut.h>
+
+void RenderScene(int temp)
+{
+}
+
+void Display(void)
+{
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // 바탕색을 'blue' 로 지정
+}
+
+void Idle(void)
+{
+}
+
+void MouseInput(int button, int state, int x, int y)
+{
+}
+
+void KeyDownInput(unsigned char key, int x, int y)
+{
+}
+
+void KeyUpInput(unsigned char key, int x, int y)
+{
+}
+
+void SpecialKeyDownInput(int key, int x, int y)
+{
+}
+
+void SpecialKeyUpInput(int key, int x, int y)
+{
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(400, 0);
+	glutInitWindowSize(800, 600);		// 프로그램 외부 사이즈 결정
+	glutCreateWindow("Game Software Engineering KPU");
+
+	/*glewInit();
+	if (glewIsSupported("GL_VERSION_3_0"))	std::cout << " GLEW Version is 3.0\n ";
+	else									std::cout << "GLEW 3.0 not supported\n ";*/
+
+	// 생성자는 이곳에
+
+	glutDisplayFunc(Display);
+	glutIdleFunc(Idle);
+	glutKeyboardFunc(KeyDownInput);	// keydown event callback
+	glutKeyboardUpFunc(KeyUpInput);	// keyup event callback
+	glutMouseFunc(MouseInput);
+	glutSpecialFunc(SpecialKeyDownInput);
+	glutSpecialUpFunc(SpecialKeyUpInput);
+
+	glutMainLoop();
+
+	return 0;
+}
---
+++ MyMinecraftClient/MyMinecraftClient/global.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include<windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
